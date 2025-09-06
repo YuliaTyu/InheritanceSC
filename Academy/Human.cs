25921cfd26c40658c768e5aa76a8b0d810aba6a1
Юля <@DESKTOP-LDN2BMM>
2025-09-06 13:31:47 +0300

    Add project files.

---
+++ Academy/Human.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    internal class Human
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+
+        public Human(string lastName, string firstName, int age)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Age = age;
+            Console.WriteLine($" HConstructor: \t {GetHashCode().ToString("X")}");
+        }
+        ~Human()
+        {
+            Console.WriteLine($" Destructor: \t {GetHashCode().ToString("X")}");
+        }
+        puplic override string ToSting()
+        {
+          return base.ToString()+ $"{ LastName} { FerstName} { Age}";
+        }
+
+
+    }
+}
---
+++ Academy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human("Swarc", "Arnold", 78);
+            Console.WriteLine(human);
+
+            Student student = new Student("Colombo","Franco", 91, "HeavyMetall",)
+        }
+    }
+}
